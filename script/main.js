49a0157c398374fe9fc852e5c6cffd4e1dc8c99a
Caroline Edgren <caroline.edgren88@gmail.com>
2023-12-11 12:55:52 +0100

    Fixerat header/footer och anpassat för scroll

---
+++ script/main.js
@@ -0,0 +1,14 @@
+// --------- PROJECT CARD MODAL ---------
+// Modal funktionalitet på förstasidan
+// Gör en koppling till varje ruta/bild
+// Ta bort länkningen från dessa rutor mot aboutsidan
+// Gör hela bilden klickbar istället för endast textrutan
+// Skapa modal som popar upp
+// Gör det möjligt att stänga modalen och återgå till förstasidan
+
+
+
+// --------- PROJECT CARD SCROLL ---------
+// Carousel-funktion på projektraden på förstasidan
+// Lägg in pilar/knappar för höger vänsterskroll
+// 
