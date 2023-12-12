46043c6452fc1100165e8e39d979cace82728cbb
Caroline Edgren <caroline.edgren88@gmail.com>
2023-12-12 14:35:19 +0100

    Corrected code that caused missmatch between buttons and JSON index

--- script/main.js
+++ script/main.js
@@ -1,56 +1,63 @@
 // --------- PROJECT CARD MODAL ---------
 // Modal funktionalitet på förstasidan
 
-
-//När jag fått modalen att fungera så la jag till en koppling till json för att minska kod i index. 
+/*När jag fick modalen att fungera på ett kort så 
+la jag till en koppling till json för att minska kod i index. */
 async function projectJson() {
-    try {
-      const response = await fetch('./json/projects.json');
-      const jsonObjects = await response.json();
-      //Skapa länk till modalen
-      const popup = document.getElementById('popup');
-        // Skapa element där min json data placeras
-      function createPopupElements(data) {
-        const titleElement = document.createElement("h2");
-        titleElement.textContent = data.projects[0].project;
-  
-        const infoElement = document.createElement("p");
-        infoElement.textContent = data.projects[0].information;
-  // Appenda de skapade elementen
-        popup.appendChild(titleElement);
-        popup.appendChild(infoElement);
-  // Stäng-knapp inuti modalen
-        const closeButton = document.createElement("button");
-        closeButton.textContent = "Close";
-        closeButton.addEventListener("click", closePopup);
-        popup.appendChild(closeButton);
-      }
-  // Öppna-knapp
-  const openButton = document.getElementById('openBtn');
-  openButton.addEventListener('click', openPopup);
+  try {
+    const response = await fetch("./json/projects.json");
+    const jsonObjects = await response.json();
+// Länka till popup-rutan
+    const popup = document.getElementById("popup");
+// Skapa text-elementen i popup
+    function createPopupElements(data) {
+      const titleElement = document.createElement("h2");
+      titleElement.textContent = data.project;
 
-//Funktion för att öppna modalen
-      function openPopup() {
-        popup.innerHTML = ''; 
-        createPopupElements(jsonObjects[0]);
-        popup.classList.add("open-popup");
-      }
-  //Funktion för att stänga modalen
-      function closePopup() {
-        popup.classList.remove("open-popup");
-      }
-  
-
-      popup.addEventListener("click", function(event) {
-        if (event.target.tagName === "BUTTON") {
-          closePopup();
-        }
-      }); // Logga error
-    } catch (error) {
-      console.error('Something went wrong');
-      console.error(error);
+      const infoElement = document.createElement("p");
+      infoElement.textContent = data.information;
+// Apenda elementen
+      popup.appendChild(titleElement);
+      popup.appendChild(infoElement);
+// Skapa och apenda stäng-knapp
+      const closeButton = document.createElement("button");
+      closeButton.textContent = "Close";
+      closeButton.addEventListener("click", closePopup);
+      popup.appendChild(closeButton);
     }
+
+    // Koppla knapparna jag lagt på korten
+    const buttons = document.querySelectorAll(".project-links");
+    /* Skrev en forEach för att kunna applicera functionerna på alla kort.
+  Stötte på problem där varje kort öppnade nästa json-index, märkt kod är med hjälp av forum */
+    buttons.forEach((button) => {
+      button.addEventListener("click", () => {
+        const projectId = button.id.slice(-1); // Med hjälp av google korrigerade jag missmatch mellan index/knappar
+        const selectedProject = jsonObjects[0].projects[projectId - 1]; // Justerade även index för att korrigera missmatch
+        openPopup(selectedProject);
+      });
+    });
+
+    // Öppna modal vid knapptryck
+    function openPopup(data) {
+      popup.innerHTML = "";
+      createPopupElements(data);
+      popup.classList.add("open-popup");
+    }
+    // Funktion för att stänga modal
+    function closePopup() {
+      popup.classList.remove("open-popup");
+    }
+    // Stäng modal vid knapptryck
+    popup.addEventListener("click", function (event) {
+      if (event.target.tagName === "BUTTON") {
+        closePopup();
+      }
+    }); // Logga eventuella fel
+  } catch (error) {
+    console.error("Something went wrong");
+    console.error(error);
   }
-  
-  projectJson();
-  
+}
+
+projectJson();
