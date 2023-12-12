7cda041dbe486fe75abe9fe6d89c2fa206fcea92
Caroline Edgren <caroline.edgren88@gmail.com>
2023-12-12 13:48:36 +0100

    Change name of fetch-function to be more clear on what it is doing

--- script/main.js
+++ script/main.js
@@ -1,52 +1,56 @@
 // --------- PROJECT CARD MODAL ---------
 // Modal funktionalitet på förstasidan
-// Gör en koppling till ruta/bild
-// Skapa modal som popar upp
+
 
 //När jag fått modalen att fungera så la jag till en koppling till json för att minska kod i index. 
-fetch('./json/projects.json')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(jsonObjects) {
-    const popup = document.getElementById('popup');
-
-    function createPopupElements(data) {
-      const titleElement = document.createElement("h2");
-      titleElement.textContent = data.projects[0].project;
-
-      const infoElement = document.createElement("p");
-      infoElement.textContent = data.projects[0].information; // Accessing project information
-
-      popup.appendChild(titleElement);
-      popup.appendChild(infoElement);
-
-      const closeButton = document.createElement("button");
-      closeButton.textContent = "Close";
-      closeButton.addEventListener("click", closePopup);
-      popup.appendChild(closeButton);
-    }
-
-    function openPopup() {
-      popup.innerHTML = '';
-      createPopupElements(jsonObjects[0]); // Passing the fetched data to createPopupElements
-      popup.classList.add("open-popup");
-    }
-
-    function closePopup() {
-      popup.classList.remove("open-popup");
-    }
-
-    const openButton = document.getElementById('openBtn');
-    openButton.addEventListener('click', openPopup);
-
-    popup.addEventListener("click", function(event) {
-      if (event.target.tagName === "BUTTON") {
-        closePopup();
+async function projectJson() {
+    try {
+      const response = await fetch('./json/projects.json');
+      const jsonObjects = await response.json();
+      //Skapa länk till modalen
+      const popup = document.getElementById('popup');
+        // Skapa element där min json data placeras
+      function createPopupElements(data) {
+        const titleElement = document.createElement("h2");
+        titleElement.textContent = data.projects[0].project;
+  
+        const infoElement = document.createElement("p");
+        infoElement.textContent = data.projects[0].information;
+  // Appenda de skapade elementen
+        popup.appendChild(titleElement);
+        popup.appendChild(infoElement);
+  // Stäng-knapp inuti modalen
+        const closeButton = document.createElement("button");
+        closeButton.textContent = "Close";
+        closeButton.addEventListener("click", closePopup);
+        popup.appendChild(closeButton);
       }
-    });
-  })
-  .catch(function(error) {
-    console.error('Something went wrong');
-    console.error(error);
-  });
+  // Öppna-knapp
+  const openButton = document.getElementById('openBtn');
+  openButton.addEventListener('click', openPopup);
+
+//Funktion för att öppna modalen
+      function openPopup() {
+        popup.innerHTML = ''; 
+        createPopupElements(jsonObjects[0]);
+        popup.classList.add("open-popup");
+      }
+  //Funktion för att stänga modalen
+      function closePopup() {
+        popup.classList.remove("open-popup");
+      }
+  
+
+      popup.addEventListener("click", function(event) {
+        if (event.target.tagName === "BUTTON") {
+          closePopup();
+        }
+      }); // Logga error
+    } catch (error) {
+      console.error('Something went wrong');
+      console.error(error);
+    }
+  }
+  
+  projectJson();
+  
